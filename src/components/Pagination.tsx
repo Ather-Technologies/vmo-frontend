7df782a4a9583b2f9614c1475ee48eaea6313604
William Largent <w.largent@athr.dev>
2024-08-30 21:35:14 -0600

    Implimented a limited page # render option to skip multiple pages, and prevent overflow on mobile browsers.

--- src/components/Pagination.tsx
+++ src/components/Pagination.tsx
@@ -33,23 +33,55 @@ function Pagination({ items, setCurrentItems, tableRowRef, paginationTag }: Pagi
         setCurrentItems(items.slice(indexOfFirstItem, indexOfLastItem));
     }, [currentPage, items, setCurrentItems, itemsPerPage]);
 
+    // Max number of page designations to show ie. page 1, 2, 3...
+    const maxPageNumbersToShow = 3;
+    // Calculate total number of pages
+    const totalPages = Math.ceil(items.length / itemsPerPage);
+    // Calculate the start and end page numbers to display
+    const startPage = Math.max(1, currentPage - Math.floor(maxPageNumbersToShow / 2));
+    // If the start page is less than the max number of pages to show, then set the end page to the max amount of page numbers to render
+    const endPage = Math.min(totalPages, startPage + maxPageNumbersToShow - 1);
+
+    // Create an array of page numbers to render
     const pageNumbers = [];
-    for (let i = 1; i <= Math.ceil(items.length / itemsPerPage); i++) {
+    for (let i = startPage; i <= endPage; i++) {
         pageNumbers.push(i);
     }
 
+    // Handles a click on a page number
     const handleClick = (event: React.MouseEvent<HTMLAnchorElement>) => {
         setCurrentPage(Number(event.currentTarget.id));
     };
 
+    // Handles a click on the first page button
+    const handleFirstClick = () => {
+        setCurrentPage(1);
+    };
+
+    // Handles a click on the previous page button
+    const handlePreviousClick = () => {
+        setCurrentPage(prev => Math.max(prev - 1, 1));
+    };
+
+    // Handles a click on the next page button
+    const handleNextClick = () => {
+        setCurrentPage(prev => Math.min(prev + 1, totalPages));
+    };
+
+    // Handles a click on the last page button
+    const handleLastClick = () => {
+        setCurrentPage(totalPages);
+    };
+
+    // Renders the page numbers into the dom
     const renderPageNumbers = pageNumbers.map(number => (
-        <li key={number} className="inline-block">
+        <li key={number} className="inline-block ml-1 mt-1">
             <a
                 href="#1"
                 id={number.toString()}
                 x-vmo-ptag={paginationTag}
                 onClick={handleClick}
-                className={`px-3 py-2 rounded-md text-sm font-medium ${currentPage === number
+                className={`p-2 rounded-md text-sm font-medium ${currentPage === number
                     ? "bg-blue-500 text-white"
                     : "text-gray-700 hover:bg-gray-200"
                     }`}
@@ -59,10 +91,6 @@ function Pagination({ items, setCurrentItems, tableRowRef, paginationTag }: Pagi
         </li>
     ));
 
-    const totalPages = Math.ceil(items.length / itemsPerPage);
-
-
-
     return (
         <div className="flex justify-between items-center mt-4 mx-4 mb-4">
             {items.length > 0 ? (
@@ -71,12 +99,32 @@ function Pagination({ items, setCurrentItems, tableRowRef, paginationTag }: Pagi
                         Showing {((currentPage - 1) * itemsPerPage) + 1} to {Math.min(currentPage * itemsPerPage, items.length)} of {items.length} entries
                     </div>
                     <ul className="flex">
-                        <li className="mr-2">
-                            <span id="x-vmo-pagination" x-vmo-ptag={paginationTag} x-vmo-itemsperpage={itemsPerPage} className="pl-4 text-sm">
+                        <li className="mr-1">
+                            <button onClick={handleFirstClick} hidden={currentPage === 1} className="p-2 rounded-md text-sm font-medium text-white hover:bg-blue-500">
+                                {"<<"}
+                            </button>
+                        </li>
+                        <li>
+                            <button onClick={handlePreviousClick} hidden={currentPage === 1} className="p-2 rounded-md text-sm font-medium text-white hover:bg-blue-500">
+                                {"<"}
+                            </button>
+                        </li>
+                        {renderPageNumbers}
+                        <li className="ml-1">
+                            <button onClick={handleNextClick} hidden={currentPage === totalPages} className="p-2 rounded-md text-sm font-medium text-white hover:bg-blue-500">
+                                {">"}
+                            </button>
+                        </li>
+                        <li className="ml-1">
+                            <button onClick={handleLastClick} hidden={currentPage === totalPages} className="p-2 rounded-md text-sm font-medium text-white hover:bg-blue-500">
+                                {">>"}
+                            </button>
+                        </li>
+                        <li className="ml-1">
+                            <span id="x-vmo-pagination" x-vmo-ptag={paginationTag} x-vmo-itemsperpage={itemsPerPage} className="pl-2 text-sm">
                                 Page {currentPage} of {totalPages}
                             </span>
                         </li>
-                        {renderPageNumbers}
                     </ul>
                 </>
             ) : (
